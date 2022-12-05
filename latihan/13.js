03dd3d0b571e18ba335562ebd8636aafa205e734
Shyy88 <aishyash.sh@gmail.com>
2022-12-05 17:18:28 +0700

    update-js

--- latihan/13.js
+++ latihan/13.js
@@ -1,24 +1,39 @@
 function fill(data, value, start, end) {
-    if (!data || !value) {
-        return 'Invalid Input'
-    }
-    let result = [];
-    while (true) {
-        for (let i = 0; i < data.length; i++) {
-            if (i >= start && i <= end) {
-                result.push(value)
-            } else if (i >= start) {
-                result.push(value)
-            } else if (!end && !start) {
-                result.push(value)
-            } else {
-                result.push(data[i])
-            }
-        }
-        break;
-    }
-    return result
-} 
+  // your code here
+  if (!data || !value) {
+    return 'Invalid Input';
+  }
+  if (!start) {
+    start = 0;
+  }
+  if (!end || end > data.length) {
+    end = data.length;
+  }
+  
+  while (start < end) { // tukar end < start jadi start < end
+    data[start] = value;
+    start++;
+  }
+  return data;
+}
+
+function fill(data, value, start, end) {
+  // your code here
+  if (!data || !value) {
+    return 'Invalid Input';
+  }
+  if (!start) {
+    start = 0;
+  }
+  if (!end || end > data.length) {
+    end = data.length;
+  }
+  for (let i = start; i < end; i++) { // ubah while jadi for loop
+    data[i] = value;
+    
+  }
+  return data;
+}
 
   console.log(fill()); 
   // Invalid input
@@ -37,39 +52,3 @@ function fill(data, value, start, end) {
   console.log(fill(["Alpha", "Beta", "Charlie", "Delta", "Echo", "Foxtrot"], "India"));
   // [ 'India', 'India', 'India', 'India', 'India', 'India' ]
 
-  function fill(data, value, start, end) {
-    // your code here
-    if (!data || !value) {
-      return 'Invalid Input';
-    }
-    if (!start) {
-      start = 0;
-    }
-    if (!end || end > data.length) {
-      end = data.length;
-    }
-    
-    while (start < end) { // tukar end < start jadi start < end
-      data[start] = value;
-      start++;
-    }
-    return data;
-  }
-
-  function fill(data, value, start, end) {
-    // your code here
-    if (!data || !value) {
-      return 'Invalid Input';
-    }
-    if (!start) {
-      start = 0;
-    }
-    if (!end || end > data.length) {
-      end = data.length;
-    }
-    for (let i = start; i < end; i++) { // ubah while jadi for loop
-      data[i] = value;
-      
-    }
-    return data;
-  }
